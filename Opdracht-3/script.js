f0283c51784aabc68854faffdca872e63313331e
MarcelGorel <66006966+MarcelGorel@users.noreply.github.com>
2021-01-12 12:16:58 +0100

    Add files via upload

---
+++ Opdracht-3/script.js
@@ -0,0 +1,25 @@
+var age = 20;
+var isFemale = true;
+var driverstatus = 'bob';
+
+if (age >= 18) {
+
+    document.write("you are welcome, you are over 18.")
+} else {
+    document.write("I am sorry you can't come in, you are under 18.")
+}
+
+if (isFemale) {
+    document.write("Female")
+} else {
+    document.write("no female")
+}
+
+if (driverstatus = "bob") {
+    document.write("driver")
+} else {
+    document.write("not allowed to drive")
+}
+
+
+
